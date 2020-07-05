97ac7ca4ef1b8e0b18bf7ffb336d848734142a98
Oki1 <oki.archer@gmail.com>
2020-07-05 20:44:36 +0200

    working

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-const char* vertexShaderSource = "#version 330 core\n"  //tells point positions
-                                 "layout (location = 0) in vec3 aPos;\n"
-                                 "void main() {\n"
-                                 "    gl_Position(aPos.x, aPos.y, aPos.z, 1.0f);\n"
-                                 "}";
+const char *vertexShaderSource = "#version 330 core\n"
+                                    "layout (location = 0) in vec3 aPos;\n"
+                                    "void main()\n"
+                                    "{\n"
+                                    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                    "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n" //tells pixel colors
                                    "out vec4 FragColor;\n"
@@ -46,23 +47,38 @@ int main(void) {
 
 
 
+    int success;
+    char infoLog[512];
 
     //COMPILE VERTEX SHADER
-    unsigned int vertexShader;
+    int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
+
+    if (!success) {
+        glGetProgramInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
     //COMPILE FRAGMENT SHADER
-    unsigned int fragmentShader;
+    int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
+    glGetProgramiv(fragmentShader, GL_LINK_STATUS, &fragmentShader);
+
+    if (!success) {
+        glGetProgramInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
     //CREATE SHADER OBJECT
     unsigned int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
-    int success
+
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
