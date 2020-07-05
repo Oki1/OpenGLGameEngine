0fe5af3ec1d29b0e22295842e7451e08ca1cec07
Oki1 <oki.archer@gmail.com>
2020-07-05 16:14:19 +0200

    created window

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+
+void error_callback(int error, const char* description) {
+    std::cerr << "Error: %s\n" << description;
+}
+
+
+int main() {
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        std::cerr << "GLFW initialization failed" << std::endl;
+    }
+
+
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "TestWindow", NULL, NULL);
+    if(!window) {
+        std::cerr << "window creation failed" << std::endl;
+    }
+
+    short i;
+    std::cin >> i;
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 1;
+}
