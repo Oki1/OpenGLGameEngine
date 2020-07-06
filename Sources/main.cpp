198410d21940386948241720bbed9f728cd013cc
Oki1 <9822592+Oki1@users.noreply.github.com>
2020-07-06 11:38:50 +0100

    Update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -28,7 +28,9 @@ int main() {
     swl::initial_window_height = 600;
     swl::initial_window_width = 800;
     swl::initial_window_title = "Test";
+    swl::background_color = {143, 215, 63, 255};
     swl::init();
+
 
 
     //COMPILE VERTEX SHADER
@@ -74,26 +76,41 @@ int main() {
 
 
     //SAVES THE VERTEX POSITIONS IN GPU MEMORY FOR FAST ACCESS
-    float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f
+    float vertices[] =
+            {0.5f,  0.5f, 0.0f,// top right
+             0.5f, -0.5f, 0.0f,// bottom right
+             -0.5f, -0.5f, 0.0f,// bottom left
+             -0.5f,  0.5f, 0.0};
+
+    unsigned int indicies[] = {
+            0,1,3, //FIRST TRIANGLE
+            1,2,3  //SECOND TRIANGLE
     };
-    unsigned int VBO, VAO; //VERTEX BUFFER OBJECT
+
+
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-
+    glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);  //STORE THE VERTICIES IN A BUFFER
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); //STORES THE INDICIES IN A BUFFER
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float),(void*)0);
-    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
 
+
+    glEnableVertexAttribArray(0);//UNBIND
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    swl::background_color = {143, 215, 63, 255};
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     
     //RENDER LOOP
     while(!glfwWindowShouldClose(swl::window)) {
@@ -105,11 +122,14 @@ int main() {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         //EVENTS AND BUFFERS
         swl::updateScreen(); //checks event triggers, updates window state and calls callback functions
     }
-    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
     return 0;
 }
