12622cf0cfd491ee1760a6c2d51b3edb072aff73
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-07-06 11:27:08 +0200

    Update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -66,7 +66,7 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float),(void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),(void*)nullptr);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
