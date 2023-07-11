2d9394f7d6b8db4b3e2eaa7c7958d68c736ae421
Oki1 <oskar.rotar@gmail.com>
2023-07-11 21:08:12 +0200

    added cube with EBO

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -10,11 +10,18 @@ pub struct Renderer {
     shader_program: gl::types::GLuint,
     vao: gl::types::GLuint,
     vbo: gl::types::GLuint,
+    ebo: gl::types::GLuint,
 }
 
 impl Renderer {
     pub fn new(display: &glutin::display::Display) -> Self {
-        const verts: [f32;9] = [-0.5, -0.5, 0.0,0.5, -0.5, 0.0,0.0,  0.5, 0.0];
+        const VERTS: [f32;12] = [-0.5, -0.5, 0.0,
+                                0.5, -0.5, 0.0,
+                                -0.5, 0.5 , 0.0,
+                                0.5, 0.5, 0.0
+                                ];
+        const INDEXES: [u32;6] = [0,1,2,
+                                1,2,3];
         const VERTEX_SHADER_SOURCE: &[u8] = b"#version 330 core\n
                                         layout (location = 0) in vec3 aPos;\n
                                         void main() {\n;
@@ -26,34 +33,54 @@ impl Renderer {
                                    out vec4 FragColor;
                                    void main() {
                                        FragColor = vec4(0.0f, 1.0f, 0.0f, 1.0f);
-                                   }
-                                   \0";
-        // setup renderer
-        //put vertext positions into vbo
-        let  shader_program;
-        let mut vao;
-        let mut vbo;
-        unsafe {
+                                    }
+                                    \0";
+                                    // setup renderer
+                                    //put vertext positions into vbo
+                                    let  shader_program;
+                                    let mut vao;
+                                    let mut vbo;
+                                    let mut ebo;
+                                    unsafe {
             vao = std::mem::zeroed();
             vbo = std::mem::zeroed();
+            ebo = std::mem::zeroed();
             //bind opengl functions
             gl::load_with(|symbol| {
                 let symbol = CString::new(symbol).unwrap();
                 display.get_proc_address(symbol.as_c_str()).cast()
             });
-            let intsize: gl::types::GLsizeiptr = (verts.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr;
-            // gen vbo
-            gl::GenBuffers(1, &mut vbo);
-            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-            gl::BufferData(gl::ARRAY_BUFFER, intsize, verts.as_ptr() as *const _, gl::STATIC_DRAW);
+            //let intsize: gl::types::GLsizeiptr = ;
+
+            // GPU STUFF
+            {
+                // gen vbo ebo vao
+                gl::GenVertexArrays(1, &mut vao);
+                gl::GenBuffers(1, &mut vbo);
+                gl::GenBuffers(1, &mut ebo);
+
+                //bind vao
+                gl::BindVertexArray(vao);
+                
+                //bind and set vbo
+                gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+                gl::BufferData(gl::ARRAY_BUFFER, (VERTS.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, VERTS.as_ptr() as *const _, gl::STATIC_DRAW);
+
+                //bind and set ebo
+                gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+                gl::BufferData(gl::ELEMENT_ARRAY_BUFFER, (INDEXES.len() * std::mem::size_of::<i32>()) as gl::types::GLsizeiptr, INDEXES.as_ptr() as *const _, gl::STATIC_DRAW);
+
+                //configure vao
+                gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, (3 * std::mem::size_of::<f32>()) as gl::types::GLsizei, 0 as *const _);
+                gl::EnableVertexAttribArray(0);
+                
+                
+            }
 
             // compile vertex shader
             let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
             gl::ShaderSource(vertex_shader, 1, [VERTEX_SHADER_SOURCE.as_ptr().cast()].as_ptr(), std::ptr::null());
             gl::CompileShader(vertex_shader);
-        
-        
-
             //compile fragment shader
             let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
             gl::ShaderSource(fragment_shader, 1, [FRAGMENT_SHADER_SOURCE.as_ptr().cast()].as_ptr(), std::ptr::null());
@@ -74,24 +101,20 @@ impl Renderer {
             //gl::DeleteShader(fragmentShader);
             gl::LinkProgram(shader_program);
         
-            // gen and bind vao
-            gl::GenVertexArrays(1, &mut vao);
-            gl::BindVertexArray(vao);
-            gl::BindBuffer(gl::ARRAY_BUFFER, vao);
-            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, (3 * std::mem::size_of::<f32>()) as gl::types::GLsizei, 0 as *const _);
-            gl::EnableVertexAttribArray(0);
         }
         Self {
-            shader_program, vao, vbo
+            shader_program, vao, vbo, ebo
         }
     }
     pub fn draw(&self, window_surface: &glutin::surface::Surface<glutin::surface::WindowSurface>, context: &glutin::context::PossiblyCurrentContext) {
         unsafe {
             gl::ClearColor(1.0f32, 0f32, 0f32, 1.0f32);
             gl::Clear(gl::COLOR_BUFFER_BIT);
+            
             gl::UseProgram(self.shader_program);
             gl::BindVertexArray(self.vao);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, 0 as *const _);
+            //gl::DrawArrays(gl::TRIANGLES, 0, 3);
 
             window_surface.swap_buffers(&context).expect("Swapping buffers failed!");
         }
