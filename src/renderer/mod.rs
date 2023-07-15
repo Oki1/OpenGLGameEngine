33adc2b9649bfefa25507c18fe41f91345288773
Oki1 <oskar.rotar@gmail.com>
2023-07-15 23:49:12 +0200

    trying to add 3d projection

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -3,6 +3,7 @@ use std::{ptr,time};
 
 use std::ffi::{CStr, CString};
 
+use gl::Viewport;
 use glutin::{display::GlDisplay,
     prelude::GlSurface};
 
@@ -13,8 +14,13 @@ pub struct Renderer {
     vao: gl::types::GLuint,
     vbo: gl::types::GLuint,
     ebo: gl::types::GLuint,
-    rot_uniform_location: gl::types::GLuint,
     time_since_last_frame: time::Instant,
+    model: glm::Mat4,
+    view: glm::Mat4,
+    projection: glm::Mat4,
+    model_uniform: gl::types::GLuint,
+    view_uniform: gl::types::GLuint,
+    projection_uniform: gl::types::GLuint,
 }
 
 impl Renderer {
@@ -30,9 +36,11 @@ impl Renderer {
 
         const VERTEX_SHADER_SOURCE: &[u8] = b"#version 330 core\n
                                         layout (location = 0) in vec3 aPos;\n
-                                        uniform mat4 rotation;\n
+                                        uniform mat4 model;\n
+                                        uniform mat4 view;\n
+                                        uniform mat4 projection;\n
                                         void main() {\n;
-                                            gl_Position = rotation * vec4(aPos, 1.0f);\n
+                                            gl_Position = projection * view * model * vec4(aPos, 1.0f);\n
                                         }\n
                                  \0";
 
@@ -48,7 +56,10 @@ impl Renderer {
         let mut vao: gl::types::GLuint;
         let mut vbo: gl::types::GLuint;
         let mut ebo: gl::types::GLuint;
-        let mut rot_uniform_location: gl::types::GLuint = 0;
+        
+        let mut model_uniform: gl::types::GLuint = 0;
+        let mut view_uniform: gl::types::GLuint = 0;
+        let mut projection_uniform: gl::types::GLuint = 0;
         
         unsafe {
             vao = std::mem::zeroed();
@@ -89,19 +100,19 @@ impl Renderer {
             let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
             gl::ShaderSource(vertex_shader, 1, [VERTEX_SHADER_SOURCE.as_ptr().cast()].as_ptr(), std::ptr::null());
             gl::CompileShader(vertex_shader);
+            
+            let mut log: [u8; 512] = [0;512];
+            
+            gl::GetShaderInfoLog(vertex_shader, 512, ptr::null::<i32>() as *mut i32, (&mut log as *mut u8) as *mut i8);
+            
+            println!("{}", std::str::from_utf8(&log as &[u8]).unwrap());
+            
             //compile fragment shader
             let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
             gl::ShaderSource(fragment_shader, 1, [FRAGMENT_SHADER_SOURCE.as_ptr().cast()].as_ptr(), std::ptr::null());
             gl::CompileShader(fragment_shader);
 
             
-            /*
-            let mut log: [u8; 512] = [0;512];
-            
-            gl::GetShaderInfoLog(fragment_shader, 512, ptr::null::<i32>() as *mut i32, (&mut log as *mut u8) as *mut i8);
-            
-            println!("{}", std::str::from_utf8(&log as &[u8]).unwrap());
-            */
             
             // create shader ob
             shader_program = gl::CreateProgram();
@@ -111,13 +122,25 @@ impl Renderer {
             gl::DeleteShader(vertex_shader);
             gl::LinkProgram(shader_program);
 
-            let rot: CString = CString::new("rotation").unwrap();
-            rot_uniform_location = gl::GetUniformLocation(shader_program, rot.as_ptr()) as u32;
-        
+            let mut s: CString = CString::new("model").unwrap();
+            model_uniform = gl::GetUniformLocation(shader_program, s.as_ptr()) as u32;
+            s = CString::new("view").unwrap();
+            view_uniform = gl::GetUniformLocation(shader_program, s.as_ptr()) as u32;
+            s = CString::new("projection").unwrap();
+            projection_uniform = gl::GetUniformLocation(shader_program, s.as_ptr()) as u32;
+            
+            
+
         }
         let time_since_last_frame = time::Instant::now();
+
+        let model = glm::identity::<f32, 4>();
+        let view = glm::translation::<f32>(glm::vec3(0.0f, 0.0f, -10.0f));
+        let projection = glm::perspective::<f32>(1920/1080.0, glm::radians(45.0f), 0.1f, 100.0f);
+        
+
         Self {
-            shader_program, vao, vbo, ebo, rot_uniform_location, time_since_last_frame
+            shader_program, vao, vbo, ebo, time_since_last_frame, model_uniform, view_uniform, projection_uniform, model, projection, view
         }
     }
     pub fn draw(&mut self, window_surface: &glutin::surface::Surface<glutin::surface::WindowSurface>, context: &glutin::context::PossiblyCurrentContext) {
@@ -126,16 +149,17 @@ impl Renderer {
             gl::Clear(gl::COLOR_BUFFER_BIT);
         }
         let now = time::Instant::now();
-        let delta_time: f32 = now.duration_since(self.time_since_last_frame).as_millis() as f32;
         //self.time_since_last_frame = now;
-        let rotation = glm::rotation::<f32>(delta_time/700.0, &glm::vec3(0.0f32, 0.0f32, -1.0f32));
         
         unsafe{
             gl::UseProgram(self.shader_program);
             gl::BindVertexArray(self.vao);
             gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, 0 as *const _);
             //gl::DrawArrays(gl::TRIANGLES, 0, 3);
-            gl::UniformMatrix4fv(self.rot_uniform_location as i32, 1, gl::FALSE, rotation.as_ptr());
+            gl::UniformMatrix4fv(self.model_uniform as i32, 1, gl::FALSE, self.model.as_ptr());
+            gl::UniformMatrix4fv(self.view_uniform as i32, 1, gl::FALSE, self.view.as_ptr());
+            gl::UniformMatrix4fv(self.projection_uniform as i32, 1, gl::FALSE, self.projection.as_ptr());
+
             window_surface.swap_buffers(&context).expect("Swapping buffers failed!");
         }
     }
