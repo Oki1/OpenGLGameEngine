b63f33c2d26747ba4b341509d6188b5642fc3289
oki1 <oki.archer@gmail.com>
2023-07-20 12:47:52 +0200

    improved error handling in loader

--- src/model/loader/mod.rs
+++ src/model/loader/mod.rs
@@ -17,43 +17,46 @@ use byteorder::{ByteOrder, LittleEndian};
 }*/
 
 // helper functions
-fn read_uint(f: &mut File) -> u32 {
+fn read_uint(f: &mut File) -> std::io::Result<u32> {
     let mut raw = [0u8;4];
-    f.read_exact(&mut raw).expect("Reading u32 from file failed!");
-    u32::from_le_bytes(raw)
+    f.read_exact(&mut raw)?;
+    Ok(u32::from_le_bytes(raw))
 }
 
-fn read_f32_array(f: &mut File, n: &usize) -> Box<[f32]> {
+fn read_f32_array(f: &mut File, n: &usize) -> std::io::Result<Box<[f32]>> {
     let mut verts_raw = vec![0u8; *n* std::mem::size_of::<f32>()];
     let mut verts_vec = vec![0f32; *n];
-    f.read_exact(&mut verts_raw).expect("Reading float array from file failed!");
+    f.read_exact(&mut verts_raw)?;
     LittleEndian::read_f32_into(&verts_raw, &mut verts_vec);
-    verts_vec.into_boxed_slice()
+    Ok(verts_vec.into_boxed_slice())
 }
 
-fn read_i32_array(f: &mut File, n: &usize) -> Box<[i32]> {
+fn read_i32_array(f: &mut File, n: &usize) -> std::io::Result<Box<[i32]>> {
     let mut indices_raw = vec![0u8; *n * std::mem::size_of::<f32>()];
     let mut indices_vec = vec![0i32; *n];
-    f.read_exact(&mut indices_raw).expect("Reading integer array from file failed!");
+    f.read_exact(&mut indices_raw)?;
     LittleEndian::read_i32_into(&indices_raw, &mut indices_vec);
-    indices_vec.into_boxed_slice()
+    Ok(indices_vec.into_boxed_slice())
 }
 
 
 //methods
+
+//pub fn load_raw_model_checked(filename: &str) -> 
+
 // unsafe as there is no check if the file is correctly structured. Indices could be out of range
 pub unsafe fn load_raw_model(filename: &str) -> std::io::Result<(Box<[f32]>, Box<[i32]>)>{
     let mut file = File::open(filename)?;
 
     // read size of verts
-    let n_verts = read_uint(&mut file);
+    let n_verts = read_uint(&mut file)?;
 
 
-    let verts = read_f32_array(&mut file, &(n_verts as usize));
+    let verts = read_f32_array(&mut file, &(n_verts as usize))?;
 
 
-    let n_indices = read_uint(&mut file);
-    let indices = read_i32_array(&mut file, &(n_indices as usize));
+    let n_indices = read_uint(&mut file)?;
+    let indices = read_i32_array(&mut file, &(n_indices as usize))?;
 
     Ok((verts, indices))
 }
