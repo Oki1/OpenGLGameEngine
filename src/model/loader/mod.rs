fc5972d34de7c9134e663c4c5eddfe5c4770b900
oki1 <oki.archer@gmail.com>
2023-07-20 12:45:25 +0200

    refactored loader code

--- src/model/loader/mod.rs
+++ src/model/loader/mod.rs
@@ -16,15 +16,32 @@ use byteorder::{ByteOrder, LittleEndian};
 
 }*/
 
+// helper functions
 fn read_uint(f: &mut File) -> u32 {
     let mut raw = [0u8;4];
-    f.read_exact(&mut raw).unwrap();
+    f.read_exact(&mut raw).expect("Reading u32 from file failed!");
     u32::from_le_bytes(raw)
 }
 
-//fn readType<T>()
+fn read_f32_array(f: &mut File, n: &usize) -> Box<[f32]> {
+    let mut verts_raw = vec![0u8; *n* std::mem::size_of::<f32>()];
+    let mut verts_vec = vec![0f32; *n];
+    f.read_exact(&mut verts_raw).expect("Reading float array from file failed!");
+    LittleEndian::read_f32_into(&verts_raw, &mut verts_vec);
+    verts_vec.into_boxed_slice()
+}
 
-// unsafe as there is no check if the file is correctly structured
+fn read_i32_array(f: &mut File, n: &usize) -> Box<[i32]> {
+    let mut indices_raw = vec![0u8; *n * std::mem::size_of::<f32>()];
+    let mut indices_vec = vec![0i32; *n];
+    f.read_exact(&mut indices_raw).expect("Reading integer array from file failed!");
+    LittleEndian::read_i32_into(&indices_raw, &mut indices_vec);
+    indices_vec.into_boxed_slice()
+}
+
+
+//methods
+// unsafe as there is no check if the file is correctly structured. Indices could be out of range
 pub unsafe fn load_raw_model(filename: &str) -> std::io::Result<(Box<[f32]>, Box<[i32]>)>{
     let mut file = File::open(filename)?;
 
@@ -32,25 +49,11 @@ pub unsafe fn load_raw_model(filename: &str) -> std::io::Result<(Box<[f32]>, Box
     let n_verts = read_uint(&mut file);
 
 
-    let verts: Box<[f32]>;
-    {
-        let mut verts_raw = vec![0u8; n_verts as usize * std::mem::size_of::<f32>()];
-        let mut verts_vec = vec![0f32; n_verts as usize];
-        file.read_exact(&mut verts_raw)?;
-        LittleEndian::read_f32_into(&verts_raw, &mut verts_vec);
-        verts = verts_vec.into_boxed_slice();
-    }
+    let verts = read_f32_array(&mut file, &(n_verts as usize));
 
 
     let n_indices = read_uint(&mut file);
-    let indices: Box<[i32]>;
-    {
-        let mut indices_raw = vec![0u8; n_indices as usize * std::mem::size_of::<f32>()];
-        let mut indices_vec = vec![0i32; n_indices as usize];
-        file.read_exact(&mut indices_raw)?;
-        LittleEndian::read_i32_into(&indices_raw, &mut indices_vec);
-        indices = indices_vec.into_boxed_slice();
-    }
+    let indices = read_i32_array(&mut file, &(n_indices as usize));
 
     Ok((verts, indices))
 }
