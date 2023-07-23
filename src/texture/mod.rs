c4a2ada853bdafdc3558e84fe7881bfd87d8c5eb
Oki1 <oskar.rotar@gmail.com>
2023-07-23 15:07:38 +0200

    added texture module

--- src/texture/mod.rs
+++ src/texture/mod.rs
@@ -16,9 +16,11 @@ impl Texture {
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T,       gl::REPEAT as i32);
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER,   gl::LINEAR_MIPMAP_LINEAR as i32);
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER,   gl::LINEAR as i32);
+
+            let ((width, height), data) = loader::load_raw_image("test_image.bin").unwrap();
+            gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGBA as i32, width as i32, height as i32, 0, gl::RGBA, gl::FLOAT, Box::into_raw(data) as *mut std::ffi::c_void);
         }
-        let ((width, height), data) = loader::load_raw_image("test_image.bin").unwrap();
-        println!("{:?}\n{:?}\n{:?}", width, height, data);
+        //println!("{:?}\n{:?}\n{:?}", width, height, data);
 
         Self {texture_id}
     }
